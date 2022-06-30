07d8dfbbd7202c79fe13af92a16b4200539e7847
MatthewPollardUK <matthewpoll@live.com>
2022-06-30 16:38:04 +0100

    working checkbox ticks and titles of recipes display in 7 day calendar div

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const searchForm = document.querySelector('#search-form');
 const searchField  = document.querySelector('input[name="search"]');
 const searchResultsContain = document.querySelector('.search-results-container');
+const sevenDayContainer = document.querySelector('.seven-day-container');
+
+
 
 
 
@@ -11,8 +14,9 @@ let searchFormSubmitted = (e) => {
   //let myresult = e.elements["search"].value;
  let searchVal = searchField.value;
  //console.log(`this is the searchval: ${searchVal}`);
+searchResultsContain.innerHTML = '';
+ dataRetrieve(searchVal);
 
- dataRetrieve(searchVal)
 }
 
 //get input from search form
@@ -25,7 +29,7 @@ const dataRetrieve = (searchVal) => {
    redirect: 'follow'
   };
 
-  let urlToUse = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=ca7d95d0&app_key=e0d18286b748e55ba5431e957ebab929&to=5`;
+  let urlToUse = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchVal}&app_id=ca7d95d0&app_key=e0d18286b748e55ba5431e957ebab929&to=5`;
 
 fetch(urlToUse, requestOptions)
  .then(response => response.text())
@@ -47,16 +51,29 @@ const displayResults = (recipies) => {
 //console.log(res);
 for (var i = 0, len = 5; i < len; i++ ){
 let caloriesRecipe = Math.floor(recipies[i].recipe.calories / recipies[i].recipe.yield);  //number
-console.log(recipies[i]);
+//console.log(recipies[i]);
 
 
 searchResultsContain.innerHTML +=
 `<div> ${recipies[i].recipe.label}    <img src=${recipies[i].recipe.images.SMALL.url} width=${recipies[i].recipe.images.SMALL.width} height=${recipies[i].recipe.images.SMALL.height} > </div>
  <div> <b> Calories: </b> ${caloriesRecipe} <a href =${recipies[i].recipe.url}> View recipe </a> </div>
- <div> col3  </div>`;
+ <div class="recipe-checkbox"> <input type="checkbox" data-checkbox="${i}" id="recipe-${i}" name="recipe-${i}"> <label for="recipe-${i}"> Add to 7 day calendar </label> </div>`;
 
-//  console.log(`Another recipe:`);
- //console.log(recipies[i]);
-}
+} // close for
+
+  let resultsCheckboxes = document.querySelectorAll('.recipe-checkbox > input[type="checkbox"]');
+     resultsCheckboxes.forEach(recipeCheckbox => recipeCheckbox.addEventListener('change', (e) => {
+       if (e.target.checked) {
+        let currentRecipe = e.target.dataset.checkbox;
+sevenDayContainer.innerHTML +=
+` <div class="recipe">${recipies[currentRecipe].recipe.label}  </div>   `
+
+} else {
+  console.log(`Checkbox is not checked: ${e.target}`);
+}  // close if else
+}) // close event listener
+) // close for each
+
+
 
 }; // close displayresults
